214086cf201fa28c6fe2fe1924dd5236ce66f1b1
mkb <anmolkb1020@gmail.com>
2023-05-07 01:49:27 +0500

    home updated

--- weather/src/Components/Navbar.js
+++ weather/src/Components/Navbar.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
-    <div class="container-fluid">
-      <Link class="navbar-brand" href="/">Weather App</Link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+    <div className="container-fluid">
+      <Link className="navbar-brand" href="/">Weather App</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <Link class="nav-link " aria-current="page" to="/">Home</Link>
-          <Link class="nav-link " aria-current="page" to="/AboutUs">About Us</Link>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link className="nav-link " aria-current="page" to="/">Home</Link>
+          <Link className="nav-link " aria-current="page" to="/AboutUs">About Us</Link>
           
         </div>
       </div>
--- weather/src/Pages/Home.js
+++ weather/src/Pages/Home.js
@@ -2,14 +2,23 @@ import React from 'react'
 
 const Home = () => {
 
- 
-  return (
-    <div  style={{height : "620px", backgroundColor : "#031633"}}>
-       <div>
-            Enter the name of city 
-       </div>
-    </div>
-  )
+
+    return (
+        <div style={{ height: "620px", backgroundColor: "#031633" }}>
+            <div className='cityName'>
+                Enter the name of city
+            </div>
+            <form className="row mt-3 justify-content-center">
+                <div className="col-auto  ">
+                    <input type="text" className="form-control" id="text" placeholder="e.g Lahore" />
+                </div>
+                <div className="col-auto ">
+                    <button type="submit" className="btn btn-primary mb-3">Check</button>
+                </div>
+            </form>
+
+        </div>
+    )
 }
 
 export default Home
