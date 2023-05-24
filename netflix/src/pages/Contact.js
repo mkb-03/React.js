a6552543083024a7aea335e97cc6bb97d6b6ccf4
mkb <anmolkb1020@gmail.com>
2023-05-24 11:46:40 +0500

    Netflix App updated

--- netflix/src/pages/Contact.js
+++ netflix/src/pages/Contact.js
@@ -17,34 +17,43 @@ const Contact = () => {
             <h1 className="mt-1 text">Contact Customer Service</h1>
           </div>
 
-          <div className="col-8 "> 
-          <h2 className="mt-1 h2">Call us from the Netflix app</h2>
+          <div className="col-8 ">
+            <h2 className="mt-1 h2">Call us from the Netflix app</h2>
             <p className='p'>
               Contacting Netflix is now easier than ever when you contact us from the Netflix app on your Android or iOS phone or tablet! Calling through the app is free - all you need is an internet or cellular connection.
             </p>
+
+            <div className='col-8 pb-4 border-bottom border-secondary'>
+              <h6 className='mt-2 h6 ' >Download the Netflix app: </h6>
+              <a  href={googleAppstore} target={"_blank"} rel='noopener noreferrer' >
+                <img src={google} className='badges' />
+              </a>
+              <a href={iosAppstore} target={"_blank"} rel='noopener noreferrer' >
+                <img src={apple} className='badgesApple' />
+              </a>
+            </div>
+
           </div>
 
-          <div className="col-4"> 
-          <b>Popular Questions</b> <br/>
-          to do </div>
+          <div className="col-4">
+            <b>Popular Questions</b> <br />
+            <ul>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            </ul>
+          </div>
         </div>
 
 
-        <div className='col-8 pb-4 border-bottom border-secondary'>
-        <h6 className='mt-2 h6 ' >Download the Netflix app: </h6>
-          <a href={googleAppstore} target={"_blank"} rel='noopener noreferrer' >
-            <img src={google} className='badges' />
-          </a>
-          <a href={iosAppstore} target={"_blank"} rel='noopener noreferrer' >
-            <img src={apple} className='badgesApple' />
-          </a>
-        </div>
+
 
       </div>
-      
+
       <div className=" col-7 mx-5 mt-4">
-      <h2 className="mt-1 h2">Call us from any phone</h2>
-      <button type="button" className="btn btn-outline-secondary mt-2 px-3">Call Us</button>
+        <h2 className="mt-1 h2">Call us from any phone</h2>
+        <button type="button" className="btn btn-outline-secondary mt-2 px-3">Call Us</button>
       </div>
 
 
