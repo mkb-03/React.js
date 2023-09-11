4d0f87b602e65ba5368cd2e44250656d8565ead0
mkb <muhammadkhizranbutt@gmail.com>
2023-09-12 02:59:18 +0500

    Updating home page weather app

--- weather/src/Pages/About.js
+++ weather/src/Pages/About.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const About = () => {
     return (
-        <div style={{ height: "620px", backgroundColor: "#031633" }}>
+        <div style={{ height: "620px", backgroundColor: "#D3D3D3" }}>
             <h2 className='pt-5 pb-5 border-bottom'>About Us</h2>
 
             <div className="container">
-                <p className="mt-3">Weather App is a weather checking app that tells you the details of weather in your desired city. When you enter the name of your city we will tell you about the following details in your city: </p>
-                <ul className="mt-3">
+                <p className="mt-3">Weather App is a weather checking app that tells you the details of weather in your desired city. When you enter the name of your city we will tell you about weather details in your city. </p>
+                {/* <ul className="mt-3">
                     <li>Current Temperature</li>
                     <li>Minimum Temperature</li>
                     <li>Maximum Temperature</li>
@@ -18,12 +18,12 @@ const About = () => {
                     <li>Humidity</li>
                     <li>Clouds</li>
                     <li>Weather</li>
-                </ul>
+                </ul> */}
 
                 <p>
                     Want to check weather details in your city ?
                     <Link to="/">
-                        <button type="submit" className="btn btn-secondary ms-3 " > Click Here </button>
+                        <button type="submit" className="btn btn-dark ms-3 " > Click Here </button>
                     </Link>
                 </p>
 
--- weather/src/Pages/Home.js
+++ weather/src/Pages/Home.js
@@ -19,16 +19,16 @@ const Home = () => {
 
     return (
         <div className='background'>
-            
-            <div className='cityName '>
-                Enter the name of city
+            <h2 className='pt-5' >Welcome to Weather App</h2>
+            <div className='cityName ' >
+                Enter name of your city
             </div>
             <form className="row g-2 mt-3 justify-content-center" onSubmit={handleSubmit}>
                 <div className="col-auto">
                     <input type="text" className="form-control" id="cityName" value={cityName} placeholder="e.g Lahore" onChange={handleOnChange} />
                 </div>
                 <div className="col-auto ">
-                    <button type="submit" className="btn btn-secondary mb-3" > Check Weather</button>
+                    <button type="submit" className="btn btn-dark mb-3" > Check Weather</button>
                 </div>
             </form>
 
