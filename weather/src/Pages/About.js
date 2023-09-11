c836d088b42958112e11c932cc86a10da3ba0009
mkb <muhammadkhizranbutt@gmail.com>
2023-09-12 03:04:53 +0500

    Updated home page on weather app

--- weather/src/Pages/About.js
+++ weather/src/Pages/About.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const About = () => {
     return (
         <div style={{ height: "620px", backgroundColor: "#D3D3D3" }}>
-            <h2 className='pt-5 pb-5 border-bottom'>About Us</h2>
+            <h2 className='pt-5 pb-5 '>About Us</h2>
 
             <div className="container">
                 <p className="mt-3">Weather App is a weather checking app that tells you the details of weather in your desired city. When you enter the name of your city we will tell you about weather details in your city. </p>
