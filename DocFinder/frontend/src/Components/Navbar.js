dc4e189da015ce8fa52bdc345a6c167a32e3e688
mkb <muhammadkhizranbutt@gmail.com>
2023-10-04 00:17:56 +0500

    working on navbar

--- DocFinder/frontend/src/Components/Navbar.js
+++ DocFinder/frontend/src/Components/Navbar.js
@@ -3,44 +3,62 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-            {/* <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg p-2 mt-1 nav-text">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">DocFinder</a>
+                    <a className="navbar-brand  " href="#">
+                        <span className='yellow-colour' >Doc</span>
+                        <span className='blue-colour'>Finder</span>
+                    </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <a className="nav-link " aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
+                                <a className="nav-link" href="#">Find Doctor</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <a className="nav-link" href="#">Contact Us</a>
                             </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About Us</a>
+                            </li>
+
                         </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+
+                        <div class="d-flex" >
+
+                            <div class="dropdown show me-2">
+                                <a class="btn btn-secondary blue-button-color " href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Register
+                                </a>
+
+                                <div class="dropdown-menu custom-dropdown-menu me-2" aria-labelledby="dropdownMenuLink">
+                                    <a class="dropdown-item" href="/patientRegistration">Patient</a>
+                                    <a class="dropdown-item" href="/doctorRegistration">Doctor</a>
+                                </div>
+                            </div>
+
+                            <div class="dropdown show me-4">
+                                <a class="btn btn-secondary blue-button-color " href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Log In
+                                </a>
+
+                                <div class="dropdown-menu custom-dropdown-menu me-2" aria-labelledby="dropdownMenuLink">
+                                    <a class="dropdown-item  " href="/patientlogin">Patient</a>
+                                    <a class="dropdown-item" href="/doctorlogin">Doctor</a>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
-            </nav> */}
+            </nav>
 
-            
+
 
         </>
     )
